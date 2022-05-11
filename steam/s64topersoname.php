4a939e5935cf04d8512fbaaf4484c7d3752be9a1
AlexDR15 <AlexDR15@users.noreply.github.com>
2022-05-12 00:00:07 +0200

    Update s64topersoname.php

--- steam/s64topersoname.php
+++ steam/s64topersoname.php
@@ -1,9 +1,10 @@
 <meta charset="UTF-8">
 <?php 
 $steamid64 = $_GET['s64'];
+$steamApiKey = $_GET['apikey'];
 
 //Sacar Nombre de Usuario de SteamID64
-$xml_steam = simplexml_load_file('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=9B03A3E22841A0B34375789454265C12&steamids='.$steamid64.'&format=xml');
+$xml_steam = simplexml_load_file('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$steamApiKey.'&steamids='.$steamid64.'&format=xml');
 $nombre_user = $xml_steam->players->player->personaname;
 //FIN Sacar Nombre de Usuario de SteamID64
 
